1770956bd9e577e392c724be7bdcedc6233b3584
mahmood1996 <mahmood.abdelrazek@outlook.com>
2023-04-17 22:32:19 +0200

    Defined WasRun Class And wasRun Flag

--- wasrun.py
+++ wasrun.py
@@ -1,7 +1,17 @@
+class WasRun:
+    def __init__(self, name: str) :
+        self.wasRun = None
+    
+    def testMethod(self) :
+        self.wasRun = 1
+
+    def run(self) :
+        self.testMethod()
+
 test = WasRun('testMethod')
 
 print(test.wasRun)
 
-test.testMethod()
+test.run()
 
 print(test.wasRun)
