93a22c4298a6dc9f36709f1c963d1b091a6ee48d
mahmood1996 <mahmood.abdelrazek@outlook.com>
2023-04-24 18:50:08 +0200

    Simplified WasRun by setting the 'wasRun' flag in setUp

--- was_run.py
+++ was_run.py
@@ -2,10 +2,10 @@ from test_case import TestCase
 
 class WasRun(TestCase) :
     def __init__(self, name: str) :
-        self.wasRun = None
         TestCase.__init__(self, name)
     
     def setUp(self) :
+        self.wasRun = None
         self.wasSetUp = 1
 
     def testMethod(self) :
