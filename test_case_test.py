5bddf6fd4df22e7e2817f5abe2b72fca0cba9310
mahmood1996 <mahmood.abdelrazek@outlook.com>
2023-04-24 18:52:34 +0200

    Simplified TestCaseTest.testRunning() not to check the flag before running the test, That is only valid if testSetUp is inplace and running correctly

--- test_case_test.py
+++ test_case_test.py
@@ -4,7 +4,6 @@ from was_run import WasRun
 class TestCaseTest(TestCase) :
     def testRunning(self) :
         test = WasRun('testMethod')
-        assert(not test.wasRun)
         test.run()
         assert(test.wasRun)
     
