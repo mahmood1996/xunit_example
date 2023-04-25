b63251da04f5c8d977750a25b1619c43bf6b9538
mahmood1996 <mahmood.abdelrazek@outlook.com>
2023-04-26 00:08:44 +0200

    Simplified the testcases checking the example testcase (WasRun)

--- test_case_test.py
+++ test_case_test.py
@@ -2,15 +2,17 @@ from test_case import TestCase
 from was_run import WasRun
 
 class TestCaseTest(TestCase) :
-    def testRunning(self) :
-        test = WasRun('testMethod')
-        test.run()
-        assert(test.wasRun)
-    
-    def testSetUp(self) :
-        test = WasRun('testMethod')
-        test.run()
-        assert(test.wasSetUp)
+    def setUp(self) :
+        self.test = WasRun('testMethod')
 
+    def testSetUp(self) :
+        self.test.run()
+        assert(self.test.wasSetUp)
+    
+    def testRunning(self) :
+        self.test.run()
+        assert(self.test.wasRun)
+    
+    
 TestCaseTest('testRunning').run()
 TestCaseTest('testSetUp').run()
